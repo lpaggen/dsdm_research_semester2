d8bd2a791808996acce91e96337ac542bc7f1fbb
Leo Paggen <lpaggen@gmail.com>
2025-03-07 13:50:36 +0100

    updates to script

--- python/arxiv_scraping.py
+++ python/arxiv_scraping.py
@@ -1,23 +1,29 @@
 import requests
 import time
 import os
+import pandas as pd
+import socks
+import socket
 
-def download_page(title, authors, year, url, output_dir = None):
-	headers = {
-        "User-Agent": "Script to download quantum computing arxiv papers (example@student.maastrichtuniversity.nl)" # replace with own email, this isn't mandatory but it's nice to let the server admin know who you are
-    }
+# fyi i used a proxy to avoid getting banned by the server
+# you can use your own proxy or remove this part if you don't need it
+socks.set_default_proxy(socks.SOCKS5, "IP", PORT, username="USR", password="PWD")
+socket.socket = socks.socksocket
 
+def download_page(title, url, output_dir = None):
+	# you would usually use a header, but we use rotating proxies so no need
+	
 	file_already_exists = False # setting this outside the try-except block avoids errors
 	
-	try:		
+	try:
 		if output_dir and not os.path.exists(output_dir):
 			os.makedirs(output_dir)
 
-		# Sanitize filename
+		# this fixes the title, otherwise we can't read the csv
 		fixed_title = title.replace("|", ",")
 		fixed_title = fixed_title.replace("}", "-")
 		filepath = os.path.join(output_dir, fixed_title + ".pdf")
-		
+
 		if not os.path.exists(output_dir):
 			os.makedirs(output_dir)
 
@@ -27,7 +33,7 @@ def download_page(title, authors, year, url, output_dir = None):
 			print(f"File {filepath} already exists. Skipping download.")
 			return
 		else: # handle the request only if the file isn't downloaded already -> don't overload server
-			response = requests.get(url, headers=headers, stream=True)  # Fetch the HTML
+			response = requests.get(url, stream=True)  # Fetch the HTML
 			response.raise_for_status() # Raise an exception for bad status codes (4xx or 5xx)
 
 		print(f"Downloading {url} to {filepath}...")
@@ -49,5 +55,24 @@ def download_page(title, authors, year, url, output_dir = None):
 		return
 	finally:
 		if not file_already_exists:
-			time.sleep(15)  # change this at own risk, this is basically the time between requests to the server as per robots.txt guidelines
+			time.sleep(1) # sleep for 1 second to avoid overloading the server !! no proxy = 15s
+			
+def process_csv(csv_filepath): # this helper function just reads the csv and calls the download_page function
+	"""Processes a CSV file containing arXiv URLs.
+
+	Args:
+		csv_filepath: The path to the CSV file.
+	"""
+	try:
+		df = pd.read_csv(csv_filepath)
+		for row in df.itertuples():
+			title, authors, date, url = row[1], row[2], row[3], row[4]
+			download_page(title, url, output_dir = os.path.join("papers/", str(date) + "/"))
+	except Exception as e:
+		print(f"An unexpected error occurred: {e}")
+
+if __name__ == "__main__":
+    csv_file = "python/fixed_arxiv_links.csv" # replace with own csv path to the processed files
+    process_csv(csv_file)
+
 # EOF
